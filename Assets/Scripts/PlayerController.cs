22c38fc04b13f563b90a2e0b25271bbc2544e5f9
mjpurin <mjpurin@gmail.com>
2025-06-10 18:45:53 +0900

    移動

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rigid2D;
+    float jumpForce = 680.0f;
+    float walkForce = 30.0f;
+    float maxWalkSpeed = 2.0f;
+
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+        rigid2D = GetComponent<Rigidbody2D>();
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigid2D.AddForce(transform.up * jumpForce);
+        }
+
+        int key = 0;
+        if (Input.GetKey(KeyCode.RightArrow)) key = 1;
+        if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
+
+        float speedx = Mathf.Abs(rigid2D.velocity.x);
+        if (speedx < maxWalkSpeed)
+        {
+            rigid2D.AddForce(transform.right * key * walkForce);
+        }
+        if (key != 0)
+        {
+            transform.localScale = new Vector3(key, 1, 1);
+        }
+        
+    }
+}
